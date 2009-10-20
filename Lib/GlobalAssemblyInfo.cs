a7574f9a62a280e552e0de9b214206376fc0e93b
Jern_cp <jesper@niedermann.dk>
2009-10-20 22:00:00 +0000

    Retro fitted AssemblyInfo to Version 0.8

---
+++ Lib/GlobalAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("KlimaKonflikters")]
+[assembly: AssemblyCopyright("Copyright © KlimaKonflikters 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.8.0.0")]
