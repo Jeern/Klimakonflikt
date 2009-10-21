5b0544c243cc0849a8433c0fcbcd324e8ccdb336
JakobLundKrarup_cp <SND\JakobLundKrarup_cp>
2009-10-21 18:15:16 +0000

    Tilføjet SceneManagement version 0.5

---
+++ GameDev.Core/SceneManagement/PauseScene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace GameDev.Core.SceneManagement
+{
+    public class PauseScene : Scene
+    {
+        public PauseScene(Game game) : base("Pause", game) {}
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime) {}
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime){}
+    }
+}
