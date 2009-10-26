7398624b775a1a71c9d9cff577a8b17936f50bcc
Jern_cp <SND\Jern_cp>
2009-10-26 17:55:12 +0000

    LE: MoveableImages no longer saved in png

---
+++ LevelEditor/MoveableImageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LevelEditor
+{
+    public static class MoveableImageController
+    {
+        private static List<MoveableImage> m_Images = new List<MoveableImage>();
+        public static List<MoveableImage> Images
+        {
+            get { return m_Images; }
+        }
+    }
+}
