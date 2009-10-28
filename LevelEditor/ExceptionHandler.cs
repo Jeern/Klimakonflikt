9a62125b4c6e9748480d59af941dbc0f9acc5fe0
Jern_cp <SND\Jern_cp>
2009-10-28 22:08:12 +0000

    ExceptionHandling added

---
+++ LevelEditor/ExceptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace LevelEditor
+{
+    public static class ExceptionHandler
+    {
+        public static void Handle(Exception ex)
+        {
+            var sb = new StringBuilder();
+            if (ex == null)
+            {
+                sb.AppendLine("Something weird happened.");
+            }
+            else
+            {
+                sb.AppendLine("Error: " + ex.Message);
+                sb.AppendLine();
+                sb.AppendLine(ex.ToString());
+            }
+            MessageBox.Show(sb.ToString(), "LevelEditor", MessageBoxButton.OK, MessageBoxImage.Stop);
+        }
+    }
+}
