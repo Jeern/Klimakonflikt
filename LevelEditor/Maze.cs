bcdbe8fd70ba36cb0f1b6db9c5ff1da68d4fd198
Jern_cp <jesper@niedermann.dk>
2009-10-29 07:41:15 +0000

    Can save levels to XML

--- LevelEditor/Maze.cs
+++ LevelEditor/Maze.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Controls;
 
 namespace LevelEditor
 {
@@ -18,6 +19,32 @@ namespace LevelEditor
             m_LevelNumber = levelNumber;
             m_LevelName = levelName;
             m_SpeedFactor = speedFactor;
+            m_VerticalImages = new Image[(int)HorizontalTiles + 1, (int)VerticalTiles];
+            m_HorizontalImages = new Image[(int)HorizontalTiles, (int)VerticalTiles + 1];
+            m_RoundedImages = new Image[(int)HorizontalTiles + 1, (int)VerticalTiles + 1];
+            m_Tiles = new Tile[(int)HorizontalTiles, (int)VerticalTiles];
+        }
+
+        //public void InitializeWallsFromTiles()
+        //{
+        //}
+
+        /// <summary>
+        /// Creates the Tiles array from the walllimages. Used when saving as XML.
+        /// </summary>
+        public static void InitializeTilesFromWalls()
+        {
+            for (int x = 0; x < HorizontalTiles; x++)
+            {
+                for (int y = 0; y < VerticalTiles; y++)
+                {
+                    m_Tiles[x, y] = new Tile();
+                    m_Tiles[x, y].Left = (m_VerticalImages[x, y].Opacity == LEConstants.Visible);
+                    m_Tiles[x, y].Right = (m_VerticalImages[x+1, y].Opacity == LEConstants.Visible);
+                    m_Tiles[x, y].Top = (m_HorizontalImages[x, y].Opacity == LEConstants.Visible);
+                    m_Tiles[x, y].Bottom = (m_HorizontalImages[x, y+1].Opacity == LEConstants.Visible);
+                }
+            }
         }
 
         private static double m_SpeedFactor;
@@ -83,7 +110,30 @@ namespace LevelEditor
             get { return Height / VerticalTiles; }
         }
 
+        private static Image[,] m_VerticalImages;
+        public static Image[,] VerticalImages
+        {
+            get { return m_VerticalImages; }
+        }
 
+        private static Image[,] m_HorizontalImages;
+        public static Image[,] HorizontalImages
+        {
+            get { return m_HorizontalImages; }
+        }
 
+        private static Image[,] m_RoundedImages;
+        public static Image[,] RoundedImages
+        {
+            get { return m_RoundedImages; }
+        }
+
+        private static Tile[,] m_Tiles;
+        public static Tile[,] Tiles
+        {
+            get { return m_Tiles; }
+        }
+
+        
     }
 }
