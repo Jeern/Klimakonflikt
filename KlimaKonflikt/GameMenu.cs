e54ffa2ffb43bd135a33bc297d90180762ff70c3
JakobLundKrarup_cp <SND\JakobLundKrarup_cp>
2009-10-31 21:03:17 +0000

    Working on game menu code

---
+++ KlimaKonflikt/GameMenu.cs
@@ -0,0 +1,57 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+using GameDev.Core;
+using GameDev.GameBoard;
+using GameDev.Core.Graphics;
+using GameDev.Core.SceneManagement;
+using GameDev.Core.Sequencing;
+using GameDev.Core.Menus;
+
+#endregion
+
+namespace KlimaKonflikt
+{
+   public class GameMenu :  MenuBase
+    {
+
+       public static readonly string MENU_STARTSINGLEPLAYER = "Start single player game"; 
+       public static readonly string MENU_START2PLAYER = "Start 2 player game";
+       public static readonly string MENU_INSTRUCTIONS = "Instructions";
+       public static readonly string MENU_CREDITS = "Credits";
+       public static readonly string MENU_EXIT = "Exit game";
+
+       SpriteFont menuFont;
+
+       TextMenuItem mnuStart2PlayerGame, mnuStart1PlayerGame, mnuCredits, mnuExit;
+
+           public GameMenu ()
+	{
+               menuFont = GameDevGame.Current.Content.Load<SpriteFont>("MenuText");
+               mnuStart2PlayerGame = new TextMenuItem(MENU_START2PLAYER, menuFont, MENU_START2PLAYER);
+               mnuStart2PlayerGame = new TextMenuItem(MENU_STARTSINGLEPLAYER, menuFont, MENU_STARTSINGLEPLAYER);
+               mnuStart2PlayerGame = new TextMenuItem(MENU_INSTRUCTIONS, menuFont, MENU_INSTRUCTIONS);
+               mnuStart2PlayerGame = new TextMenuItem(MENU_CREDITS, menuFont, MENU_CREDITS);
+               mnuStart2PlayerGame = new TextMenuItem(MENU_START2PLAYER, menuFont, MENU_START2PLAYER);
+	}
+
+        public override void ArrangeMenuItems()
+        {
+
+        }
+    }
+}
--- KlimaKonflikt/Scenes/SceneNames.cs
+++ KlimaKonflikt/Scenes/SceneNames.cs
@@ -7,10 +7,11 @@ namespace KlimaKonflikt.Scenes
 {
     public static class SceneNames
     {
-        public static readonly string INTROSCENE = "Intro scene";
+        public static readonly string INTROSCENE = "Intro";
+        public static readonly string MENUSCENE = "Menu";
         public static readonly string MAINSCENE = "Main scene";
         public static readonly string OILWINSCENE = "Oil wins";
         public static readonly string FLOWERWINSCENE = "Flower wins";
-        public static readonly string CREDITSSCENE = "Credits scene";
+        public static readonly string CREDITSSCENE = "Credits";
     }
 }
