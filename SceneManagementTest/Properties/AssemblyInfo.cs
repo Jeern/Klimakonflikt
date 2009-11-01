1d3eef86568624c101c524ecd8ebfb83e5a73d26
Jern_cp <jesper@niedermann.dk>
2009-11-01 19:37:51 +0000

    AssemblyInfo corrected

--- SceneManagementTest/Properties/AssemblyInfo.cs
+++ SceneManagementTest/Properties/AssemblyInfo.cs
@@ -6,28 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SceneManagementTest")]
-[assembly: AssemblyProduct("SceneManagementTest")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
-
-[assembly: AssemblyCopyright("Copyright ©  2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("SceneManagementTest")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ff9a50e4-0ed4-46a5-9125-db910e1b2895")]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
