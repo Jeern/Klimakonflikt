f5762eee7f0cc058aad30206decc97c73e9a6018
Jern_cp <SND\Jern_cp>
2009-11-19 21:49:12 +0000

    GameDev.GameBoard refactored to make silverlight transition easier

--- GameDev.GameBoard/GameBoardPath.cs
+++ GameDev.GameBoard/GameBoardPath.cs
@@ -1,27 +1,6 @@
-﻿#region Usings
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics;
-
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
-using Microsoft.Xna.Framework.Storage;
-
 using GameDev.Core;
-using GameDev.GameBoard;
-using GameDev.Core.Graphics;
-using GameDev.Core.SceneManagement;
-using GameDev.Core.Sequencing;
-
-#endregion
-
 
 namespace GameDev.GameBoard
 {
