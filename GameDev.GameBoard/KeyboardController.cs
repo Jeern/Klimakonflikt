a05e5dafebda6402145eb82dbbfd69fa73cb5d21
Jern_cp <jesper@niedermann.dk>
2009-11-19 21:49:12 +0000

    GameDev.GameBoard refactored to make silverlight transition easier

--- GameDev.GameBoard/KeyboardController.cs
+++ GameDev.GameBoard/KeyboardController.cs
@@ -1,21 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
-using Microsoft.Xna.Framework.Storage;
-
+﻿using System.Collections.Generic;
 using GameDev.Core;
-using GameDev.GameBoard;
-using GameDev.Core.Graphics;
-using System.Diagnostics;
-using GameDev.Core.Control;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace GameDev.GameBoard
 {
